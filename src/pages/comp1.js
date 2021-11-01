2438353de71073635dad8525a47a9d82fc7532b5
Phillip <87886559+algoSupport@users.noreply.github.com>
2021-11-01 18:42:14 +0700

    initial commit

--- src/pages/comp1.js
+++ src/pages/comp1.js
@@ -2,10 +2,10 @@ import React from "react";
 import Staking from "../components/Staking";
 import { Container } from "@chakra-ui/react";
 
-export default function Comp1 () {
-	return (
-		<Container pt={10}>
-			<Staking />
-		</Container>
-	)
+export default function Comp1() {
+  return (
+    <Container maxW="container.lg" pt={10}>
+      <Staking />
+    </Container>
+  );
 }
